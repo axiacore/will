9194ea318f1f254f4c066567e1e0ef0b414c5775
Camilo Nova <camilo.nova@axiacore.com>
2015-03-25 10:54:29 -0500

    Added random fun

--- plugins/axiacore.py
+++ plugins/axiacore.py
@@ -1,3 +1,4 @@
+import random
 import requests
 
 from will import settings
@@ -32,6 +33,19 @@ class AxiaCorePlugin(WillPlugin):
         doc = pq(url='http://devopsreactions.tumblr.com/random')
         self.say(doc('.post_title').text(), message=message)
         self.say(doc('.item img').attr('src'), message=message)
+
+    @randomly(
+        start_hour='9',
+        end_hour='17', day_of_week='mon-fri', num_times_per_day=2
+    )
+    def random_fun(self, message):
+        req = requests.get(
+            'http://www.reddit.com/r/holdmybeer/top/.json?sort=top&t=week'
+        )
+        if req.ok:
+            elem = random.choice(req.json()['data']['children'])
+            self.say(elem['data']['url'], message=message)
+            self.say(elem['data']['title'], message=message)
 
     @require_settings('DOOR_URL')
     @respond_to('open the door')
