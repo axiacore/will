dbc018602f8a3ab6e5736e1bc28580df6d3c78a6
Camilo Nova <camilo.nova@axiacore.com>
2015-03-31 09:49:55 -0500

    Test

--- plugins/axiacore.py
+++ plugins/axiacore.py
@@ -43,8 +43,8 @@ class AxiaCorePlugin(WillPlugin):
         else:
             self.reply(message, 'I could not open the door', color='red')
 
-    @randomly(start_hour='9', end_hour='16', day_of_week="mon-fri", num_times_per_day=20)
-    def fun_talk(self):
+    @randomly(start_hour='9', end_hour='16', day_of_week="mon-fri", num_times_per_day=40)
+    def funrandom(self):
         req = requests.get(
             'http://www.reddit.com/r/holdmybeer/top/.json?sort=top&t=week',
             headers={'User-Agent': 'Mozilla/5.0'},
