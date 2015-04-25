853a7521fa33b925b8c65c86ee269e9b00da5736
Camilo Nova <camilo.nova@axiacore.com>
2015-04-25 14:06:24 -0500

    Improve url pattern

--- plugins/linode_plugin.py
+++ plugins/linode_plugin.py
@@ -15,7 +15,7 @@ class LinodePlugin(WillPlugin):
     }
 
     @require_settings('LINODE_API_KEY')
-    @respond_to('^linode (?P<command>.*) (?P<machine>.*)')
+    @respond_to('^linode (?P<command>\w+)(?: (?P<machine>\w+))?')
     def linode_command(self, message, command=None, machine=None):
         """
         Get a list of available linodes.
@@ -28,12 +28,13 @@ class LinodePlugin(WillPlugin):
             self.say(
                 'Give a command like: %s' % ', '.join(command_list),
                 message=message,
+                color='red',
             )
 
         linode_api = api.Api(settings.LINODE_API_KEY)
+        linode_list = self.load('linode_list', {})
 
         if command == 'status':
-            linode_list = self.load('linode_list', {})
             for linode in linode_api.linode_list():
                 linode_list[linode['LABEL']] = {
                     'id': linode['LINODEID'],
@@ -44,3 +45,11 @@ class LinodePlugin(WillPlugin):
             self.say(rendered_template('linode_list.html', {
                 'linode_list': linode_list,
             }), message=message, html=True)
+
+        elif command == 'reboot':
+            if machine not in linode_list:
+                self.say(
+                    '%s does not exist' % machine,
+                    message=message,
+                    color='red',
+                )
