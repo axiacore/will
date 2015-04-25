44aac5942822deec2f0fe1340c1f1aa42ae479a0
Camilo Nova <camilo.nova@axiacore.com>
2015-04-25 13:58:26 -0500

    Improve command management

--- plugins/linode_plugin.py
+++ plugins/linode_plugin.py
@@ -7,28 +7,40 @@ from linode import api
 
 class LinodePlugin(WillPlugin):
 
+    STATUS = {
+        -1: 'Being Created',
+        0: 'Brand New',
+        1: 'Running',
+        2: 'Powered Off',
+    }
+
     @require_settings('LINODE_API_KEY')
-    @respond_to('^linode list$')
-    def linode_list(self, message):
+    @respond_to('^linode (?P<command>.*) (?P<machine>.*)')
+    def linode_command(self, message, command=None, machine=None):
         """
         Get a list of available linodes.
         """
-        status = {
-            -1: 'Being Created',
-            0: 'Brand New',
-            1: 'Running',
-            2: 'Powered Off',
-        }
+        command_list = (
+            'status',
+            'reboot',
+        )
+        if command not in command_list:
+            self.say(
+                'Give a command like: %s' % ', '.join(command_list),
+                message=message,
+            )
+
         linode_api = api.Api(settings.LINODE_API_KEY)
 
-        linode_list = self.load('linode_list', {})
-        for linode in linode_api.linode_list():
-            linode_list[linode['LABEL']] = {
-                'id': linode['LINODEID'],
-                'status': status[linode['STATUS']],
-            }
-        self.save('linode_list', linode_list)
+        if command == 'status':
+            linode_list = self.load('linode_list', {})
+            for linode in linode_api.linode_list():
+                linode_list[linode['LABEL']] = {
+                    'id': linode['LINODEID'],
+                    'status': self.STATUS[linode['STATUS']],
+                }
+            self.save('linode_list', linode_list)
 
-        self.say(rendered_template('linode_list.html', {
-            'linode_list': linode_list,
-        }), message=message, html=True)
+            self.say(rendered_template('linode_list.html', {
+                'linode_list': linode_list,
+            }), message=message, html=True)
