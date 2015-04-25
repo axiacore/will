fdfaf39637b52c7c618ce1be949f4164647f2514
Camilo Nova <camilo.nova@axiacore.com>
2015-04-25 13:44:55 -0500

    Save on memory the linode list

--- plugins/linode_plugin.py
+++ plugins/linode_plugin.py
@@ -1,6 +1,6 @@
 from will import settings
 from will.plugin import WillPlugin
-from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+from will.decorators import respond_to, rendered_template, require_settings
 
 from linode import api
 
@@ -10,8 +10,22 @@ class LinodePlugin(WillPlugin):
     @require_settings('LINODE_API_KEY')
     @respond_to('^linode list$')
     def linode_list(self, message):
+        status = {
+            '-1': 'Being Created',
+            '0': 'Brand New',
+            '1': 'Running',
+            '2': 'Powered Off',
+        }
         linode_api = api.Api(settings.LINODE_API_KEY)
 
+        linode_list = self.load('linode_list', {})
+        for linode in linode_api.linode_list():
+            linode_list[linode['LABEL']] = {
+                'id': linode['LINODEID'],
+                'status': status[linode['STATUS']],
+            }
+        self.save('linode_list', linode_list)
+
         self.say(rendered_template('linode_list.html', {
-            'linode_list': linode_api.linode_list(),
-            }), message=message, html=True)
+            'linode_list': linode_list,
+        }), message=message, html=True)
