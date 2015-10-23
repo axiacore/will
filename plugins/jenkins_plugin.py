47845cb459cedae3f535fc510c29b872ac26b91b
Camilo Nova <camilo.nova@axiacore.com>
2015-10-23 15:28:32 -0500

    Added jenkins build

--- plugins/jenkins_plugin.py
+++ plugins/jenkins_plugin.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: AxiaCore S.A.S. http://axiacore.com
+import time
+
 from will import settings
 from will.plugin import WillPlugin
 from will.decorators import respond_to, rendered_template, require_settings
@@ -18,10 +20,10 @@ class JenkinsPlugin(WillPlugin):
         return
 
     @require_settings('JENKINS_URL', 'JENKINS_USER', 'JENKINS_TOKEN')
-    @respond_to('^jenkins list$')
+    @respond_to('^jenkins jobs$')
     def jenkins_list(self, message):
         """
-        Get a list of available jenkins jobs: jenkins list
+        Get a list of available jenkins jobs: jenkins jobs
         """
         self.say(
             message=message,
@@ -59,3 +61,79 @@ class JenkinsPlugin(WillPlugin):
             html=True,
             notify=True,
         )
+
+    @require_settings('JENKINS_USER', 'JENKINS_TOKEN')
+    @respond_to('^jenkins build (?P<label>[-\w]+)$')
+    def jenkins_build(self, message, label):
+        """
+        Build a jenkins job: jenkins build axiacore-website-testing
+        """
+        self.say(
+            message=message,
+            content="I'm building a jenkins job. Hang in there bro...",
+        )
+
+        jenkins_list = self.load('jenkins_list', {})
+
+        if label not in jenkins_list:
+            return self.__return_error(
+                message=message,
+                content='Job %s does not exist' % label,
+            )
+
+        response = requests.post(
+            jenkins_list[label]['url'] + 'build',
+            auth=(settings.JENKINS_USER, settings.JENKINS_TOKEN),
+            verify=False,
+        )
+        if response.ok:
+            job = jenkins_list[label]['name']
+            self.reply(
+                message=message,
+                content='%s is now building.' % job,
+                color='yellow',
+            )
+
+            polling_url = response.headers['Location']
+            while True:
+                response = requests.post(
+                    polling_url,
+                    auth=(settings.JENKINS_USER, settings.JENKINS_TOKEN),
+                    verify=False,
+                )
+                if response.ok and response.json()['task']['color']:
+                    color = response.json()['task']['color']
+                    if color.endswith('_anime'):
+                        time.sleep(2)
+                    elif color == 'blue':
+                        self.reply(
+                            message=message,
+                            content='%s build success. %s' % (
+                                job,
+                                response.json()['executable']['url'],
+                            ),
+                            color='green',
+                            notify=True,
+                        )
+                        return
+                    elif color == 'red':
+                        self.reply(
+                            message=message,
+                            content='%s build failed. %s' % (
+                                job,
+                                response.json()['executable']['url'],
+                            ),
+                            color='red',
+                            notify=True,
+                        )
+                        return
+                    else:
+                        return self.__return_error(
+                            message=message,
+                            content='Status %s not recognized' % color,
+                        )
+                else:
+                    return self.__return_error(
+                        message=message,
+                        content='Cannot get build result.',
+                    )
